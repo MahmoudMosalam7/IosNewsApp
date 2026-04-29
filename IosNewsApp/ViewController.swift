463ade5d848a38ace8fd7ae0c4f23b16c8986780
mahmoudmosalamdev <Mahmoud.Mosalam.dev@gmail.com>
2026-04-29 16:21:46 +0300

    Initial Commit

---
+++ IosNewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IosNewsApp
+//
+//  Created by Mahmoud Mosalam on 29/04/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
