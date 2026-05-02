c0cd557564358eb92b7b2ed8c9750b6c0d5d1a1b
mahmoudmosalamdev <Mahmoud.Mosalam.dev@gmail.com>
2026-05-02 16:32:46 +0300

    build : create cell for countries

--- IosNewsApp/Component/HorizontalCountriesButtonCollectionViewCell.swift
+++ IosNewsApp/Component/HorizontalCountriesButtonCollectionViewCell.swift
@@ -8,5 +8,14 @@
 import UIKit
 
 class HorizontalCountriesButtonCollectionViewCell: UICollectionViewCell {
+    private let label = UILabel()
     
+    func Setup(country : String){
+        contentView.backgroundColor = .systemGray5
+        contentView.layer.cornerRadius = 10
+        label.textAlignment = .center
+        label.text = country
+        contentView.addSubview(label)
+        label.frame = contentView.bounds
+    }
 }
