dee9d41446b900fbc00748c60cea5398764e34c5
mahmoudmosalamdev <Mahmoud.Mosalam.dev@gmail.com>
2026-05-02 21:48:11 +0300

    build : hnadle navigation from main screen  to details screen

---
+++ IosNewsApp/NewsDetails/NewsDetailsVC.swift
@@ -0,0 +1,29 @@
+//
+//  NewsDetailsVC.swift
+//  IosNewsApp
+//
+//  Created by Mahmoud Mosalam on 02/05/2026.
+//
+
+import UIKit
+
+class NewsDetailsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
